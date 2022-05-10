a937a694f09a06f8ef099a264849ee9c24398f09
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-10 16:20:26 -0400

    added functionality for feature candidates, resetting database, creating a new one, seeding the table and selecting information afterwards with a new connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
 /* creating a table for cadidates, id is auto incremented, has category for first/last name 30 characters long, industry - connected */
+
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
